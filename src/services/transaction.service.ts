fbcb0bd5ac99e36b8a6ef8e03e5b468aa686d8ca
Yofannnn <yofanapril17@gmail.com>
2025-01-20 19:29:04 +0700

    update: replace midtrans fetch func direct to transaction service and interface to transaction type. delete: midtrans folder and init file

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -2,22 +2,33 @@
 
 import connectToDatabase from "@/database/mongoose";
 import TransactionModel from "@/database/models/transaction.model";
-import { getMidtransTransactionDetails, requestMidtransTransaction } from "@/midtrans/init";
 import { TransactionSchema } from "@/validations/transaction.validation";
+import { InitializeTransactionPayload, MidtransTransactionDetails } from "@/types/transaction.type";
 
-export async function initializeTransactionService(payload: any) {
+const serverKey = process.env.MIDTRANS_SERVER_KEY;
+if (!serverKey) throw new Error("MIDTRANS_SERVER_KEY is not defined");
+const auth = Buffer.from(serverKey + ":").toString("base64");
+
+/**
+ * Initialize a new transaction by storing the transaction to the database and
+ * storing a new transaction to midtrans and get the midtrans payment token.
+ *
+ * @param {InitializeTransactionPayload} payload - The payload for initializing a new transaction.
+ * @returns {Promise<{ success: boolean; message: string; data: { transaction: Transaction; midtransPaymentToken: string } | null }>} - The result of the initialization.
+ *   If success is true, the data is the new transaction and the midtrans payment token, otherwise the data is null.
+ */
+export async function initializeTransactionService(payload: InitializeTransactionPayload) {
   try {
     const validationBody = await TransactionSchema.safeParseAsync({
       ...payload,
       transactionTime: new Date(payload.transactionTime),
     });
 
-    if (!validationBody.success) return { success: false, message: "Sorry, your data is invalid" };
+    if (!validationBody.success) return { success: false, message: validationBody.error };
 
     const { orderId, grossAmount } = validationBody.data;
-
-    await connectToDatabase();
-
+    const serverKey = process.env.MIDTRANS_SERVER_KEY as string;
+    const auth = Buffer.from(serverKey + ":").toString("base64");
     const requestMidtransTransactionPayload = {
       transaction_details: {
         order_id: orderId,
@@ -26,53 +37,69 @@ export async function initializeTransactionService(payload: any) {
       credit_card: { secure: true },
     };
 
-    // get midtrans payment token
-    const midtransPaymentToken = await requestMidtransTransaction(requestMidtransTransactionPayload);
-    if (!midtransPaymentToken.success)
-      return {
-        success: false,
-        message: midtransPaymentToken.errorMessage || "Midtrans payment token error",
-      };
-
-    // store transaction in database
-    const transaction = await TransactionModel.create({
-      ...validationBody.data,
-      transactionId: midtransPaymentToken.data?.token,
+    const requestMidtransTransaction = await fetch(process.env.MIDTRANS_REQUEST_TRANSACTION_URL as string, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic " + auth,
+      },
+      body: JSON.stringify(requestMidtransTransactionPayload),
     });
+    if (!requestMidtransTransaction.ok) throw new Error(requestMidtransTransaction.statusText);
+
+    const midtransPaymentToken = (await requestMidtransTransaction.json()) as { token: string; redirect_url: string };
+
+    await connectToDatabase();
+
+    const transaction = await TransactionModel.create({ ...validationBody.data, transactionId: midtransPaymentToken.token });
     if (!transaction) return { success: false, message: "Transaction error" };
 
     return {
       success: true,
       message: "success",
-      data: { transaction, midtransPaymentToken: midtransPaymentToken.data?.token },
+      data: { transaction, midtransPaymentToken: midtransPaymentToken.token },
     };
   } catch (error: any) {
     return { success: false, message: error.message };
   }
 }
 
+/**
+ * Settlement a transaction by updating the transaction status to "settlement" in the database
+ * and storing the midtrans transaction details to the database.
+ *
+ * @param {string} orderId - The ID of the transaction to settle.
+ * @returns {Promise<{ success: boolean; message: string; data: Transaction | null }>} - The result of the settlement.
+ *   If success is true, the data is the updated transaction, otherwise the data is null.
+ */
 export async function settlementTransactionService(orderId: string) {
   try {
-    const midtransTransactionDetails = await getMidtransTransactionDetails(orderId);
+    const url = `https://api.sandbox.midtrans.com/v2/${orderId}/status`;
+    const responseMidtransTransactionDetails = await fetch(url, {
+      method: "GET",
+      headers: { "Content-Type": "application/json", Authorization: "Basic " + auth },
+    });
+    if (!responseMidtransTransactionDetails.ok) throw new Error(responseMidtransTransactionDetails.statusText);
 
-    if (midtransTransactionDetails.data?.transaction_status !== "settlement") throw new Error("Transaction not valid");
+    const midtransTransactionDetails = (await responseMidtransTransactionDetails.json()) as MidtransTransactionDetails;
+
+    if (midtransTransactionDetails.transaction_status !== "settlement") throw new Error("Transaction not valid");
 
     const updateTransactionPayload = {
-      currency: midtransTransactionDetails.data?.currency,
-      vaNumbers: midtransTransactionDetails.data?.va_numbers,
-      paymentAmounts: midtransTransactionDetails.data?.payment_amounts.map((paymentAmount) => ({
+      currency: midtransTransactionDetails.currency,
+      vaNumbers: midtransTransactionDetails.va_numbers,
+      paymentAmounts: midtransTransactionDetails.payment_amounts.map((paymentAmount) => ({
         amount: Number(paymentAmount.amount),
         paidAt: paymentAmount.paid_at,
       })),
-      paymentType: midtransTransactionDetails.data?.payment_type,
-      transactionStatus: midtransTransactionDetails.data?.transaction_status,
-      settlementTime: midtransTransactionDetails.data?.settlement_time,
+      paymentType: midtransTransactionDetails.payment_type,
+      transactionStatus: midtransTransactionDetails.transaction_status,
+      settlementTime: midtransTransactionDetails.settlement_time,
     };
 
     const updateTransaction = await TransactionModel.findOneAndUpdate({ orderId }, updateTransactionPayload, {
       new: true,
     });
-
     if (!updateTransaction) throw new Error(updateTransaction);
 
     return { success: true, message: "success", data: updateTransaction };
@@ -81,6 +108,16 @@ export async function settlementTransactionService(orderId: string) {
   }
 }
 
+/**
+ * Get a list of transactions that match the given filter criteria.
+ *
+ * @param {object} [filter] - The filter criteria.
+ * @param {string[]} [filter.orderIds] - The orderIds to filter by.
+ * @param {string} [filter.orderType] - The orderType to filter by.
+ * @param {string} [filter.paymentPurpose] - The paymentPurpose to filter by.
+ * @param {string} [filter.transactionStatus] - The transactionStatus to filter by.
+ * @returns {Promise<Transaction[]>} - The list of transactions that match the filter criteria.
+ */
 export async function getFilteredTransaction({
   orderIds,
   orderType,
--- src/types/transaction.type.ts
+++ src/types/transaction.type.ts
@@ -34,6 +34,16 @@ export enum TransactionPaymentPurpose {
   PAID = "paid",
 }
 
+export interface InitializeTransactionPayload {
+  orderId: string;
+  orderType: TransactionOrderType;
+  grossAmount: number;
+  paymentPurpose: TransactionPaymentPurpose;
+  transactionStatus: TransactionStatus;
+  transactionTime: Date;
+  paymentType?: string | undefined;
+}
+
 export type TransactionSuccess = {
   status_code: number;
   status_message: string;
@@ -46,3 +56,22 @@ export type TransactionSuccess = {
   fraud_status: "accept" | "deny" | "challenge";
   finish_redirect_url: string;
 };
+
+export interface MidtransTransactionDetails {
+  status_code: string;
+  transaction_id: string;
+  gross_amount: string;
+  currency: string;
+  order_id: string;
+  payment_type: string;
+  signature_key: string;
+  transaction_status: string;
+  fraud_status: string;
+  status_message: string;
+  merchant_id: string;
+  va_numbers: { bank: string; va_number: string }[];
+  payment_amounts: { amount: string; paid_at: string }[];
+  transaction_time: string;
+  settlement_time: string;
+  expiry_time: string;
+}
